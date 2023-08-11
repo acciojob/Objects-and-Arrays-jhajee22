b42b844d6164a6e00cae549136e071709ae4848a
Saurav Kumar Jha <122507083+jhajee22@users.noreply.github.com>
2023-08-11 21:51:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,10 +5,26 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create a reference to the 'players' array using the 'team' variable
+const team = window.players;
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// Create a shallow copy of the 'players' array using the 'team1' variable
+const team1 = [...window.players];
+
+// Create a shallow copy of the 'person' object using the 'cap1' variable
+const cap1 = { ...window.person };
+
+// Testing the changes
+console.log("Original players:", window.players);
+console.log("Team:", team);
+console.log("Team1:", team1);
+console.log("Original person:", window.person);
+console.log("Cap1:", cap1);
+
+// Modifying the 'team' array and 'cap1' object
+team.push("Sam");
+cap1.name = "Jane";
+
+// Testing changes after modification
+console.log("Modified Team:", team);
+console.log("Modified Cap1:", cap1);
