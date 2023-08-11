c4bf8ff6f4461bed57cec3b33db2e574fa6ea76b
Saurav Kumar Jha <122507083+jhajee22@users.noreply.github.com>
2023-08-11 21:56:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
 const person = {
   name: "John Doe",
   age: 80,
 };
 
 // Create a reference to the 'players' array using the 'team' variable
-const team = window.players;
+const team = players;
 
 // Create a shallow copy of the 'players' array using the 'team1' variable
-const team1 = [...window.players];
+const team1 = [...players];
 
 // Create a shallow copy of the 'person' object using the 'cap1' variable
-const cap1 = { ...window.person };
+const cap1 = { ...person };
 
 // Testing the changes
-console.log("Original players:", window.players);
+console.log("Original players:", players);
 console.log("Team:", team);
 console.log("Team1:", team1);
-console.log("Original person:", window.person);
+console.log("Original person:", person);
+console.log("Cap1:", cap1);
+
+// Modifying the 'team' array and 'cap1' object
+team.push("Sam");
+cap1.name = "Jane";
+
+// Testing changes after modification
+console.log("Modified Team:", team);
+console.log("Modified Cap1:", cap1);
+
 console.log("Cap1:", cap1);
 
 // Modifying the 'team' array and 'cap1' object
